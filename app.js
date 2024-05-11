9ef2bcac0929739f476e3234c6d5faaf9f01b939
danimisme <msubhanr53@gmail.com>
2024-05-11 20:44:03 +0700

    create method to update contact

--- app.js
+++ app.js
@@ -120,6 +120,47 @@ app.get("/contact/edit/:id", async (req, res) => {
   });
 });
 
+//proses ubah data contact
+app.put(
+  "/contact",
+  [
+    body("nama").custom(async (value, { req }) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (value !== req.body.oldName && duplikat) {
+        throw new Error("Contact sudah terdaftar");
+      }
+      return true;
+    }),
+    check("email", "Email tidak valid").isEmail(),
+    check("nohp", "Nomor Handphone tidak valid").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("edit-contact", {
+        layout: "layouts/main-layout",
+        title: "Edit Contact",
+        errors: errors.array(),
+        contact: req.body,
+      });
+    } else {
+      Contact.updateOne(
+        { _id: req.body.id },
+        {
+          $set: {
+            nama: req.body.nama,
+            email: req.body.email,
+            nohp: req.body.nohp,
+          },
+        }
+      ).then((result) => {
+        req.flash("msg", "Data contact berhasil diubah");
+        res.redirect("/contact");
+      });
+    }
+  }
+);
+
 //Halaman Detail Contact
 app.get("/contact/:id", async (req, res) => {
   const contact = await Contact.findById(req.params.id);
