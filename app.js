90a563f2f244d090d35b623007d095e55f9c8940
danimisme <msubhanr53@gmail.com>
2024-05-11 17:46:35 +0700

    create contact list  page

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+
+require("./utils/db");
+const Contact = require("./models/contact");
+
 const app = express();
 const port = 3000;
 
@@ -9,6 +16,18 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+//konfigurasi flash
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 //Halaman Home
 app.get("/", (req, res) => {
@@ -23,6 +42,17 @@ app.get("/about", (req, res) => {
   res.render("about", {
     layout: "layouts/main-layout",
     title: "About",
+  });
+});
+
+//Halaman Contact
+app.get("/contact", async (req, res) => {
+  const contacts = await Contact.find();
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Contact List",
+    contacts,
+    msg: req.flash("msg"),
   });
 });
 
