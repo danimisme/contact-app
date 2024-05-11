066535f43562958e4c357557dd0b47ded8897cc9
danimisme <msubhanr53@gmail.com>
2024-05-11 21:28:55 +0700

    connect to mongodb atlas

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const url = process.env.MONGODB_CONNECT_URL;
+console.log(url);
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/contactApp", {
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
