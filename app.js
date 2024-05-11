4beb4c4505684fe9212064d413df0497f727201e
danimisme <msubhanr53@gmail.com>
2024-05-11 15:10:51 +0700

    Set up view engine using expressLayout

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
 const app = express();
 const port = 3000;
 
+//Set up View Engine
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+//Halaman Home
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", {
+    layout: "layouts/main-layout",
+    title: "Home",
+  });
 });
 
 app.listen(port, () => {
