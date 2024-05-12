48fcfeadee1b3fd00d93ee7fff8d771d68fb8ef9
danimisme <msubhanr53@gmail.com>
2024-05-12 12:40:16 +0700

    fix dirpath  views

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 
 const { body, validationResult, check } = require("express-validator");
@@ -23,6 +24,8 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("layout", path.join(__dirname, "views/layouts"));
 
 //konfigurasi flash
 app.use(cookieParser("secret"));
